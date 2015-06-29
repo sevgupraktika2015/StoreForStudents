a17d2ae92965df066ca72af865a040b453dc9bcf
agrigul <agriguletskiy@alvioneurope.ru>
2015-06-29 20:46:28 +0300

    first commit

---
+++ StorForStudentsWebApp/StorForStudentsWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StorForStudentsWebApp.MvcApplication" Language="C#" %>
---
+++ StorForStudentsWebApp/StorForStudentsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StorForStudentsWebApp.Startup))]
+namespace StorForStudentsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
