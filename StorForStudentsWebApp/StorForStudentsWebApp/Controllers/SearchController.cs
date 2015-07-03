21af874f4493a254f7e2d786bab6f849113fd442
Надежда <sporyshnadiya@gmail.com>
2015-07-03 17:03:43 +0300

    addup

---
+++ StorForStudentsWebApp/StorForStudentsWebApp/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DomainLogic.Repositories;
+using Implementation.Repositories;
+using StorForStudentsWebApp.Models;
+
+namespace StorForStudentsWebApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        public ActionResult Index(string searchString)
+        {
+            var itemsDbo = new List<ItemModel>();
+            IItemsRepository repository;
+            using (var context = new StoreDbContext())
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    repository = new ItemsRepository(context);
+                    var items = repository.GetAll();
+                    itemsDbo.AddRange(items.Select(item => new ItemModel(item)));
+                    itemsDbo = itemsDbo.Where(s => s.Name.Contains(searchString)).ToList();
+                }
+                else
+                {
+                    repository = new ItemsRepository(context);
+                    var items = repository.GetById(1);
+                    itemsDbo.Add(new ItemModel(items));
+                }
+            return View(itemsDbo);
+        }
+	}
+}
---
+++ StorForStudentsWebApp/StorForStudentsWebApp/Models/ItemModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DomainLogic.Entities;
+
+namespace StorForStudentsWebApp.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Name { get;  set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Item ConvertToItem()
+        {
+            Item item = new Item(Name, Price, Quantity, Id);
+            return item;
+        }
+
+        public ItemModel()
+        {
+
+        }
+
+        public ItemModel(Item item)
+        {
+            Id = item.Id;
+            Name = item.Name;
+            Price = item.Price;
+            Quantity = item.Quantity;
+        }
+    }
+}
