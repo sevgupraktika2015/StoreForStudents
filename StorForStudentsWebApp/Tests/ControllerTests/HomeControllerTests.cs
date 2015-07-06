dc98950ab3d4b76c295c05751dff13212b70fede
StarWind <andrew.starwind@gmail.com>
2015-07-06 20:07:31 +0300

    added test

--- StorForStudentsWebApp/Tests/ControllerTests/HomeControllerTests.cs
+++ StorForStudentsWebApp/Tests/ControllerTests/HomeControllerTests.cs
@@ -1,23 +1,50 @@
 ﻿using System;
-using System.Web;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StorForStudentsWebApp.Controllers;
 using StorForStudentsWebApp.Models;
+using DomainLogic.Entities;
+using Implementation.Repositories;
 
 namespace Tests.ControllerTests
 {
     [TestClass]
     public class HomeControllerTests
     {
-        /*[TestMethod]    // тест не проходит и проходить не будет, вот
-        public void ControllerTest_null_notNull()
+        [TestMethod]
+        public void EditControllerTest_Id_notNull()
         {
+            using (StoreDbContext context = new StoreDbContext())
+            {
+                ItemsRepository repository = new ItemsRepository(context);
+                Item item = new Item("item", 12, 12, 1, "desc", "IP");
+                repository.AddItem(item);
+            }
+
             var controller = new HomeController();
+            var result = controller.Edit(1) as ViewResult;
+            var product = (ItemModel) result.ViewData.Model;
 
-            var result = controller.Details(-1) as ActionResult;
+            Assert.IsNotNull(product.Name);
+            Assert.AreEqual(product.Name, "item");
+        }
 
-            Assert.AreEqual(result.RouteValues["action"], "Index");
-        }*/
+        [TestMethod]
+        public void DetailsControllerTest_Id_notNull()
+        {
+            using (StoreDbContext context = new StoreDbContext())
+            {
+                ItemsRepository repository = new ItemsRepository(context);
+                Item item = new Item("item", 12, 12, 1, "desc", "IP");
+                repository.AddItem(item);
+            }
+
+            var controller = new HomeController();
+            var result = controller.Details(1) as ViewResult;
+            var product = (ItemModel)result.ViewData.Model;
+
+            Assert.IsNotNull(product.Name);
+            Assert.AreEqual(product.Name, "item");
+        }
     }
 }
