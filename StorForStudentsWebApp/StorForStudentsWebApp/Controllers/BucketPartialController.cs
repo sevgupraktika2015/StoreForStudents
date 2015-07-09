1306ff4ce8c12a62679c8ad3cd25d1bf9d87e41b
Egrace <Egrace@yandex.ru>
2015-07-09 18:03:49 +0300

    Added bucket information into top menu.

---
+++ StorForStudentsWebApp/StorForStudentsWebApp/Controllers/BucketPartialController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StorForStudentsWebApp.Controllers
+{
+    public class BucketPartialController : Controller
+    {
+        public int GetItemsCountInOrder()
+        {
+            int itemsCount = 0;
+            //TODO: подсчет количества предметов
+            return itemsCount;
+        }
+
+        public decimal GetOrderPrice()
+        {
+            decimal orderPrice = 0;
+            //TODO: подсчет общей цены
+            return orderPrice;
+        }
+    }
+}
